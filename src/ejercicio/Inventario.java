31ed1e39a4c2202e78ffd2c384284a8f38fc3330
alvarobarrerotanarro <barreroalvaro2007@gmail.com>
2026-03-03 16:27:04 +0100

    Metodos vaciar, insertarMascota y eliminarMascota

--- src/ejercicio/Inventario.java
+++ src/ejercicio/Inventario.java
@@ -1,5 +1,37 @@
 package ejercicio;
 
-public class Inventario {
+import java.util.*;
 
+public class Inventario {
+	private List<Mascotas> contenedor;
+
+	public Inventario() {
+		vaciar();
+	}
+
+	public void vaciar() {
+		this.contenedor = new ArrayList<>();
+	}
+
+	public void insertarMascota(Mascotas a) {
+		this.contenedor.add(a);
+	}
+
+	public void eliminarMascota(String nombre) {
+		Mascotas m = null;
+		boolean found = false;
+
+		// Buscamos la mascota.
+		for (int i = 0; !found && i < contenedor.size(); i++) {
+			m = contenedor.get(i);
+			if (m.getNombre().equals(nombre)) {
+				found = true;
+			}
+		}
+
+		// Si la hemos encontrado la eliminamos.
+		if (found) {
+			contenedor.remove(m);
+		}
+	}
 }
