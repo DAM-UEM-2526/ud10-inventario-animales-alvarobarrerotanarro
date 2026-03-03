c9ebfdf8cf9eec741dd48bbb6c485720d0777881
alvarobarrerotanarro <barreroalvaro2007@gmail.com>
2026-03-03 16:21:03 +0100

    Getters y setters para Mascotas

--- src/ejercicio/Mascotas.java
+++ src/ejercicio/Mascotas.java
@@ -16,6 +16,38 @@ public abstract class Mascotas {
 		this.fechaNacimiento = fechaNacimiento;
 	}
 
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getEdad() {
+		return edad;
+	}
+
+	public void setEdad(int edad) {
+		this.edad = edad;
+	}
+
+	public String getEstado() {
+		return estado;
+	}
+
+	public void setEstado(String estado) {
+		this.estado = estado;
+	}
+
+	public String getFechaNacimiento() {
+		return fechaNacimiento;
+	}
+
+	public void setFechaNacimiento(String fechaNacimiento) {
+		this.fechaNacimiento = fechaNacimiento;
+	}
+
 	@Override
 	public String toString() {
 		return "Mascotas [nombre=" + nombre + ", edad=" + edad + ", estado=" + estado + ", fechaNacimiento="
